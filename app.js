bc9afc4747080033dd54f52c63017171ea49d737
Aviral Singh <aviralsingh@Avirals-MacBook-Air.local>
2021-01-20 18:20:04 +0530

    new changes

--- app.js
+++ app.js
@@ -144,4 +144,7 @@ app.get("/login", passport.authenticate("twitter"))
 
 
 
-app.listen(3000)
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('Express server listening on port', port)
+});
